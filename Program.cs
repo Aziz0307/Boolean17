062120211b7a58dd6edd055f0702fc1ebfd901a9
Aziz0307 <Aziz03072018@gmail.com>
2024-11-24 13:07:08 +0500

    Boolean 17

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Boolean17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Vvedite nechetnoe trechznachnoe chislo: ");
+            int a = int.Parse(Console.ReadLine());
+            bool result =(a>=100 && a<1000 && a % 2 !=0) ? true: false;
+            Console.WriteLine(result);
+        }
+    }
+}
